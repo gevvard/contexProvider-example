b3078509684496fe149fbb15498347f596c92da8
Admin <gevorgmatem@gmail.com>
2023-08-24 21:58:08 +0400

    Contex-provider-example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useContext} from "react";
+import AuthContext from "./Store/Auth-context";
+import MainHeader from "./Components/MainHeader/MainHeader";
+import Login from "./Components/Login/Login";
+import Home from "./Components/Home/Home";
+
 
 function App() {
+  const ctx = useContext(AuthContext);
+  console.log("ctx",ctx)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainHeader />
+      <main>
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
+      </main>
+    </>
   );
 }
 
